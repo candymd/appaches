32a081ef2a27c3d0365090cd94e63d2e5e3b0756
FabyBroc <fabybroc@hotmail.com>
2022-03-14 10:56:01 +0100

    modificado el footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './Footer.css'
 
 
-
 const Footer = () => {
 
 return (
 
     <div className="footer">
-        <h1>Hola</h1>
-        <span className="material-icons">face</span>
+        <i className="fa-solid fa-circle-plus" ></i>
+        <h6>Añadir Evento</h6>
+
+
+
     </div>
 )
 
