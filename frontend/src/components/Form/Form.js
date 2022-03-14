7cf8a43ce639bca9ef584dcbbf4ce582429a1a86
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-03-14 15:51:00 +0100

    implementando formulario-fecha

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -3,10 +3,16 @@ import './Form.css'
 
 function Form() {
 return (
-    <h1>Esto es para el formulario</h1>
+    <>
+    <h1>AÑADIR</h1>
+    <div className="input-group-row-3">
+        <div>
+            <label htmlFor="">Fecha</label>
+            <input  type="date" id="date"  name="experience_date" className=""/>
+        </div>
+    </div>
+    </>
 )
-
-
 }
 export default Form
 
