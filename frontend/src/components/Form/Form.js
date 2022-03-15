5e4f63072eaede35ddba477432da64ca153b29d0
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-03-15 12:38:58 +0100

    formulario, implementada estuctura

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -4,13 +4,41 @@ import './Form.css'
 function Form() {
 return (
     <>
-    <h1>AÑADIR</h1>
-    <div className="input-group-row-3">
-        <div>
-            <label htmlFor="">Fecha</label>
-            <input  type="date" id="date"  name="experience_date" className=""/>
+        <button>Done</button>
+        <h1>AÑADIR</h1>
+        <div className="input-group-row-3">
+            <div>
+                <label htmlFor="">FECHA</label>
+                <input type="date" id="date" name="experience_date" className=""/>
+            </div>
+            <div>
+                <label htmlFor="">NOMBRE DEL GASTO</label>
+                <input id="ticket-name" required name="user_name" className=""/>
+            </div>
+            <div>
+                <label htmlFor="price">IMPORTE</label>
+                <input type="number" id="import" name="import" className="form-price"/>
+            </div>
+            <div>
+                <label htmlFor="">PAGADO POR</label>
+                <button>Por ti</button>
+                <button>Por otro</button>
+            </div>
+            <div>
+                <label htmlFor="">NOMBRE</label>
+                <input id="friend-name" required name="user_name" className=""/>
+            </div>
+            <div>
+                <textarea cols="30" rows="5" className="text"></textarea>
+            </div>
+            <div>
+                <label htmlFor="">AMIGOS</label>
+                <input type="text" name="username" className="text"/>
+            </div>
+            <div>
+                <p>TOTAL APACHAS= 5e/cada participante</p>
+            </div>
         </div>
-    </div>
     </>
 )
 }
