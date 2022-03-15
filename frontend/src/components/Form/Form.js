e32668435f333b09cc038254618e258900cd7b52
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-03-15 15:55:32 +0100

    implementando estilo a button-select

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -7,34 +7,35 @@ return (
         <div className="container">
             <button className="button-icon"><i className="fa-solid fa-circle-check"></i></button>
             <h1>AÑADIR</h1>
-            <div className="input-group-row-3">
+            <div className="form">
                 <div>
                     <label htmlFor="">FECHA</label>
-                    <input type="date" id="date" name="experience_date" className=""/>
+                    <input type="date" id="date" name="experience_date" className="input"/>
                 </div>
                 <div>
                     <label htmlFor="">NOMBRE DEL GASTO</label>
-                    <input id="ticket-name" required name="user_name" className=""/>
+                    <input id="ticket-name" required name="user_name" className="input"/>
                 </div>
                 <div>
                     <label htmlFor="price">IMPORTE</label>
-                    <input type="number" id="import" name="import" className="form-price"/>
+                    <input type="number" id="import" name="import" className="input"/>
                 </div>
                 <div>
                     <label htmlFor="">PAGADO POR</label>
-                    <button>Por ti</button>
-                    <button>Por otro</button>
+                    <button className="button-select">Por ti</button>
+                    <button className="button-select">Por otro</button>
                 </div>
                 <div>
                     <label htmlFor="">NOMBRE</label>
-                    <input id="friend-name" required name="user_name" className=""/>
+                    <input id="friend-name" required name="user_name" className="input"/>
                 </div>
                 <div>
-                    <textarea cols="30" rows="5" className="text"></textarea>
+                    <label htmlFor="">LISTA</label>
+                    <textarea  className="text"></textarea>
                 </div>
                 <div>
                     <label htmlFor="">AMIGOS</label>
-                    <input type="text" name="username" className="text"/>
+                    <input type="text" name="username" className="input"/>
                 </div>
                 <div>
                     <p>TOTAL APACHAS= 5e/cada participante</p>
