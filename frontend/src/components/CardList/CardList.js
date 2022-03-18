970c52d4240cf9b433e06731acf0c9a26dbb9795
Candy <candymdeoleo@gmail.com>
2022-03-18 14:46:56 +0100

    pasando prop a aCobrarCard para borrar bill

--- frontend/src/components/CardList/CardList.js
+++ frontend/src/components/CardList/CardList.js
@@ -9,7 +9,7 @@ const CardList = ({eventsACobrar, eventsAPagar, vistaACobrar, deleteBill}) => {
     return (
 
           <div className="cardList">
-              {vistaACobrar ? eventsACobrar.map(bill => <ACobrarCard deleteBill={deleteBill} friends={bill.friends} event={bill.event} key={bill.id}/>)
+              {vistaACobrar ? eventsACobrar.map(bill => <ACobrarCard deleteBill={deleteBill} friends={bill.friends} bill={bill} event={bill.event} key={bill.id}/>)
               : eventsAPagar.map(bill => <APagarCard friends={bill.friends} event={bill.event} key={bill.id} bill={bill}/>)}
         </div>
     );
