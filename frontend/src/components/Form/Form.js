13b44a9e99d1c1b5401666033057af871de77538
FabyBroc <fabybroc@hotmail.com>
2022-03-18 09:12:14 +0100

    changes overwritten

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -70,69 +70,6 @@ const Form = (props) => {
                         <input value={registryData.amount} onChange={handleInputChange} type="text"
                                name="import" className="input"/>
                     </div>
-
-<<<<<<< HEAD
-                    <div>
-                        <label htmlFor="">PAGADO POR</label>
-                        <button onClick=
-                                    {irACobrar} className={isGreenActive ? 'green active' : 'green'}>POR MI
-                        </button>
-                        <button onClick=
-                                    {irAPagar} className={!isGreenActive ? 'red active' : 'red'}>POR OTRO
-                        </button>
-                    </div>
-                    <div>
-                        <label htmlFor="">NOMBRE</label>
-                        <input value={registryData.friends} onChange={handleInputChange} type="text" required
-                               name="user_name" className="input"/>
-                    </div>
-                    <div>
-                        <label htmlFor="">LISTA</label>
-                        <textarea className="text"></textarea>
-                    </div>
-                    <div>
-                        <p className="input">AMIGOS: ${registryData.numberFriends}</p>
-                        <p className="input">TOTAL APACHAS= 5 €/cada uno</p>
-=======
-        const handleInputChange = (event) => {
-            console.log(event.target.value)
-            setRegistryData({
-                ...registryData,
-                [event.target.name]: event.target.value
-            })
-        }
-
-        const enviarDatos = (event) => {
-            event.preventDefault()
-            props.onSubmit(registryData)
-            props.onClose()
-            navigate("/")
-
-        }
-
-        return (
-            <>
-                <div className="container">
-                    <button type="submit" className="button-icon" data-toggle="" data-target=""><Link
-                        to="/">{props.registryData ? 'Guardar cambios' :
-                        <i className="fa-solid fa-circle-check"></i>}</Link></button>
-                    <h1>AÑADIR </h1>
-                    <div className="form" onSubmit={enviarDatos} action="">
-                        <div>
-                            <label htmlFor="">FECHA</label>
-                            <input value={registryData.date} onChange={handleInputChange} type="date"
-                                   name="registry_date" className="input"/>
-                        </div>
-                        <div>
-                            <label htmlFor="">NOMBRE DEL GASTO</label>
-                            <input onChange={handleInputChange} type="text" className="input"/>
-                        </div>
-                        <div>
-                            <label htmlFor="price">IMPORTE</label>
-                            <input value={registryData.amount} onChange={handleInputChange} type="text"
-                                   name="import" className="input"/>
-                        </div>
-
                         <div>
                             <label htmlFor="">PAGADO POR</label>
                             <button onClick=
@@ -155,10 +92,9 @@ const Form = (props) => {
                             <p className="input">AMIGOS: ${registryData.numberFriends}</p>
                             <p className="input">TOTAL APACHAS= 5 €/cada uno</p>
                         </div>
->>>>>>> 892db0323b90f7626332fe4fd4c267805b3a1c28
                     </div>
                 </div>
-            </div>
+
         </>
     )
 }
