4f3b8cd23700c3ee6ff6ace16631dd34f4942777
Candy <candymdeoleo@gmail.com>
2022-03-21 08:54:57 +0100

    merge main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import './components/Footer/Footer'
 import './components/Header/Header'
-import './components/Welcome/Welcome'
+
 import {useEffect, useState} from "react";
 import Form from "./components/Form/Form";
 import {Route, Routes} from "react-router-dom";
 import {MainPage} from "./components/MainPage/MainPage";
-import Welcome from "./components/Welcome/Welcome"
+
+
 function App() {
 
     const [bills, setBills] = useState([]);
@@ -25,26 +26,21 @@ function App() {
         }
     }, [requiresUpdate])
 
-
-    const addRegistry = (newRegistry) => {
-        fetch("http://localhost:8080/registries/add",
+    const deleteBill = (id) => {
+        fetch(`http://localhost:8080/bills/delete/${id}`,
             {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(newRegistry)
+                method: 'GET'
             }
         ).then(_ => setRequiresUpdate(true))
 
     }
+
     return (
     <div className="App">
         <Routes>
-
-            <Route path="/" exact element={<MainPage registrosACobrar={registrosACobrar} registrosAPagar={registrosAPagar} vistaACobrar={vistaACobrar} setVistaACobrar={setVistaACobrar}/>}/>
-            <Route path="/form" element={<Form registrosACobrar={registrosACobrar} registrosAPagar={registrosAPagar} onSubmit={e => addRegistry(e)}/>}/>
-            <Route path ="/welcome" element={<Welcome/>}/>
+            <Route path="/" exact element={<MainPage deleteBill={deleteBill} eventsACobrar={eventsACobrar} eventsAPagar={eventsAPagar} vistaACobrar={vistaACobrar} setVistaACobrar={setVistaACobrar}/>}/>
+            <Route path="/form" element={<Form/>}/>
             </Routes>
-
     </div>
   );
 }
