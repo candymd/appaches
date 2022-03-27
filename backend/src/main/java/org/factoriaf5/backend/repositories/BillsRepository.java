4e90f97bd9c2937e0f3b971c77db7907520168e1
Candy <candymdeoleo@gmail.com>
2022-03-27 21:06:04 +0200

    anotacionn transactional

--- backend/src/main/java/org/factoriaf5/backend/repositories/BillsRepository.java
+++ backend/src/main/java/org/factoriaf5/backend/repositories/BillsRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface BillsRepository extends JpaRepository<Bills, Long> {
     List<Bills> findAllByFriendsId(Long id);
     List<Bills> findAllByEventId(Long id);
