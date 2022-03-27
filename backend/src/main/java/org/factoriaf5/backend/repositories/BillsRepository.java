245c01ef0f4eaed94c897958e995d74453cb6336
Candy <candymdeoleo@gmail.com>
2022-03-27 21:49:22 +0200

    driver error

--- backend/src/main/java/org/factoriaf5/backend/repositories/BillsRepository.java
+++ backend/src/main/java/org/factoriaf5/backend/repositories/BillsRepository.java
@@ -9,7 +9,6 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-@Transactional
 public interface BillsRepository extends JpaRepository<Bills, Long> {
     List<Bills> findAllByFriendsId(Long id);
     List<Bills> findAllByEventId(Long id);
