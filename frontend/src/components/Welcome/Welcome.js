1c2e0422a314962c177048518afd7ad6319153aa
Sbarrachina <sandrabarrachina1978@gmail.com>
2022-03-28 09:14:42 +0200

    welcome boton y bienvenida

--- frontend/src/components/Welcome/Welcome.js
+++ frontend/src/components/Welcome/Welcome.js
@@ -1,20 +1,34 @@
 import React  from "react";
 import './Welcome.css'
-import logo from './assets/logo.png'
+import logolila from './assets/logolila.png'
+import {bar, comida, compras, hogar} from "../../assets/category-icons/category-icons";
 
 const Welcome = () => {
 
     return (
 
-            <div className="WelcomeHello">
-
-                <img className= "logo-Welcome" src={logo} />
-                <h1 className="slogan"> CUANDO CADA CÉNTIMO CUENTA </h1>
-
+        <div className="WelcomeHello">
+            <img className="logo-Welcome" src={logolila}/>
+            <div className="slogan">
+                <h3> PORQUE CADA CÉNTIMO CUENTA </h3>
             </div>
+            <div className="imagenes-categorias">
+                <img  src={comida}/>
+                <img  src={compras}/>
+                <img  src={hogar}/>
+                <img  src={bar}/>
+            </div>
+            <div>
+                <button className="button-next">Siguiente</button>
+            </div>
+            <div className="texto-intro">
+                <h4>Lleva un registro de todos tus gastos compartidos con amigos, compañeros de salidas, viajes, de
+                    piso y familia.</h4>
+            </div>
+        </div>
 
 
-        )
+    )
 };
 
 export default Welcome;
